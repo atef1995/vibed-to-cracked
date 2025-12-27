54ede402eaa81b6a0f1d04ea84843cb62f7ec058
Atef Moazzen <68507159+atef1995@users.noreply.github.com>
2025-12-27 20:15:28 +0100

    feat: enhance CodeEditor with new button functionalities and improve accessibility

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -9,6 +9,8 @@ interface ButtonProps {
   uniqueKey?: string | number;
   loading?: boolean;
   id?: string;
+  title?: string;
+  disabled?: boolean;
 }
 
 const Button = ({
@@ -19,11 +21,14 @@ const Button = ({
   uniqueKey,
   loading,
   id,
+  title,
+  disabled
 }: ButtonProps) => {
   return (
     <button
+      title={title}
       id={id}
-      disabled={loading}
+      disabled={loading ?? disabled}
       key={uniqueKey}
       onClick={onClick}
       className={`flex items-center justify-center gap-3 px-6 py-3 rounded-lg font-semibold active:scale-95 cursor-pointer transition-all disabled:opacity-80 disabled:bg-gray-500 disabled:hover:bg-gray-500 disabled:cursor-wait touch-manipulation min-h-[48px] relative ${color} ${className}`}
